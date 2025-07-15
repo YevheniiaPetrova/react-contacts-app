f862e0cb40d8f8a3ddcfee3758659650722fbec8
Eugenia Lime <EugeniaLime@Eugenias-MacBook-Air.local>
2025-07-15 19:08:05 +0200

    updated contacts data

--- src/components/contacts-data.js
+++ src/components/contacts-data.js
@@ -1,33 +1,94 @@
- const Contacts = [{
-        firstName: "Барней",
-        lastName: "Стинсовський",
-        phone: "+380956319521",
-        gender: "male"
-    }, {
-        firstName: "Робін",
-        lastName: "Щербатська",
-        phone: "+380931460123",
-        gender: "female"
-    }, {
-        firstName: "Анонімний",
-        lastName: "Анонімус",
-        phone: "+380666666666",
-        gender: "none"
-    }, {
-        firstName: "Лілія",
-        lastName: "Олдровна",
-        phone: "+380504691254",
-        gender: "female"
-    }, {
-        firstName: "Маршен",
-        lastName: "Еріксонян",
-        phone: "+380739432123",
-        gender: "male"
-    }, {
-        firstName: "Теодор",
-        lastName: "Мотсбес",
-        phone: "+380956319521",
-        gender: "male"
-     }];
+ const Contacts = [
+  {
+    firstName: "Emma",
+    lastName: "Müller",
+    phone: "+4915123456789",
+    gender: "female"
+  },
+  {
+    firstName: "Liam",
+    lastName: "Schneider",
+    phone: "+498912345678",
+    gender: "male"
+  },
+  {
+    firstName: "Olivia",
+    lastName: "Johnson",
+    phone: "+12025550123",
+    gender: "female"
+  },
+  {
+    firstName: "Noah",
+    lastName: "Fischer",
+    phone: "+436601234567",
+    gender: "male"
+  },
+  {
+    firstName: "Sophia",
+    lastName: "Smith",
+    phone: "+14155552671",
+    gender: "female"
+  },
+  {
+    firstName: "Benjamin",
+    lastName: "Koch",
+    phone: "+497116543210",
+    gender: "male"
+  },
+  {
+    firstName: "Isabella",
+    lastName: "Weber",
+    phone: "+41791234567",
+    gender: "female"
+  },
+  {
+    firstName: "Ethan",
+    lastName: "Meier",
+    phone: "+496921234567",
+    gender: "male"
+  },
+  {
+    firstName: "Ava",
+    lastName: "Brown",
+    phone: "+16175550123",
+    gender: "female"
+  },
+  {
+    firstName: "Lucas",
+    lastName: "Schulz",
+    phone: "+494012345678",
+    gender: "male"
+  },
+  {
+    firstName: "Mia",
+    lastName: "Zimmermann",
+    phone: "+436641234567",
+    gender: "female"
+  },
+  {
+    firstName: "Alexander",
+    lastName: "Taylor",
+    phone: "+12025551234",
+    gender: "male"
+  },
+  {
+    firstName: "Emily",
+    lastName: "Becker",
+    phone: "+4917623456789",
+    gender: "female"
+  },
+  {
+    firstName: "Henry",
+    lastName: "Hoffmann",
+    phone: "+498921234567",
+    gender: "male"
+  },
+  {
+    firstName: "Charlotte",
+    lastName: "White",
+    phone: "+14165550111",
+    gender: "female"
+  }
+];
     
 export default Contacts;
